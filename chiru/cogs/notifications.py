ac68079f1387e14b474be7b1b8eaba66e94cf8ab
SunDwarf <sun@veriny.tf>
2016-07-04 14:20:05 +0100

    Add help texts.

--- chiru/cogs/notifications.py
+++ chiru/cogs/notifications.py
@@ -27,11 +27,17 @@ class Notifications(object):
 
     @notifications.command(pass_context=True)
     async def on(self, ctx: Context):
+        """
+        Turn notifications on.
+        """
         await ctx.set_config("notifications", "on")
         await self.bot.say("Notifications turned on.")
 
     @notifications.command(pass_context=True)
     async def off(self, ctx: Context):
+        """
+        Turn notifications off.
+        """
         await ctx.set_config("notifications", "off")
         await self.bot.say("Notifications turned off.")
 
@@ -41,7 +47,7 @@ class Notifications(object):
 
     async def on_member_ban(self, member: discord.Member):
         if (await self.bot.get_config(member.server, "notifications")) == "on":
-            await self.bot.send_message(member.server.default_channel, "**{}** got :hammer:".format(member.name))
+            await self.bot.send_message(member.server.default_channel, "**{}** got bent".format(member.name))
 
     async def on_member_unban(self, server: discord.Server, user: discord.User):
         if (await self.bot.get_config(server, "notifications")) == "on":
