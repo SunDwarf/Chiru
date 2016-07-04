8d1d893f22feb4bada58a589e641a392a079cc34
SunDwarf <sun@veriny.tf>
2016-07-04 17:11:49 +0100

    Add PUSH event.

--- chiru/cogs/commits/event_handlers.py
+++ chiru/cogs/commits/event_handlers.py
@@ -7,6 +7,7 @@ from kyokai import Request
 
 from bot import Chiru
 
+
 async def load_channels(bot: Chiru, repo: str):
     """
     Load the channels to send the bot to.
@@ -33,6 +34,30 @@ async def ping(bot: Chiru, r: Request):
         assert isinstance(channel, discord.Channel)
         await bot.send_message(channel, formatted)
 
+
+async def push(bot: Chiru, r: Request):
+    """
+    PUSH event.
+    """
+    repo = r.form["repository"]["full_name"]
+    # not the prettiest format
+    header = "**{}:** **{}** pushed *{}* commit{} to branch **{}**" \
+        .format(repo, r.form["pusher"]["name"], len(r.form["commits"]), 's' if len(r.form["commits"]) != 1 else '',
+                r.form["ref"].split("/")[-1])
+    for commit in r.form["commits"]:
+        fmt = "`{}` {} [{}]".format(commit["id"][0:7], commit["message"], commit["author"]["username"])
+        header += "\n" + fmt
+
+    header += "\n<{}>".format(r.form["compare"])
+
+    channels = await load_channels(bot, repo)
+    for channel in channels:
+        if not channel:
+            continue
+        await bot.send_message(channel, header)
+
+
 handlers = {
-    "ping": ping
+    "ping": ping,
+    "push": push
 }
