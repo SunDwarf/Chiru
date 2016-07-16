245a2a778607e9f938f0ae86faf758bbf19cf09b
SunDwarf <sun@veriny.tf>
2016-07-16 14:49:22 +0100

    Add `find` command to find in recent messages.

--- chiru/cogs/fun.py
+++ chiru/cogs/fun.py
@@ -2,10 +2,12 @@
 Fun commands.
 """
 import functools
+import re
 
 import aiohttp
 import discord
 import google
+from dateutil.parser import parse
 from discord.ext import commands
 from io import BytesIO
 
@@ -175,6 +177,74 @@ Mutual servers: {mut}```"""
 
         await self.bot.say(final_c)
 
+    @commands.command(pass_context=True)
+    async def find(self, ctx: Context, regex: str, limit: int = 100, date: str = None):
+        """
+        Search the last <limit> messages for messages matching the regex <regex>, starting from <date>.
+
+        Note that the maximum limit is 100, but you can use <date> to search for messages earlier than that.
+
+        This will return, at most, 100 messages, which will then be sent in a private message in chunks of 10 (so 50
+        messages at maximum).
+        """
+        if date is not None:
+            try:
+                date = parse(date)
+            except ValueError:
+                await self.bot.say(":x: Date could not be parsed.")
+                return None
+
+        regexp = re.compile(regex)
+
+        messages = []
+        # Make sure the limit is not above 100, to prevent endpoint spam.
+        if limit > 100:
+            limit = 100
+
+        iterator = self.bot.logs_from(ctx.channel, limit=limit, before=date)
+        async for message in iterator:
+            assert isinstance(message, discord.Message)
+            if message.author == ctx.server.me:
+                continue
+            if len(messages) >= 100:
+                break
+            if not message.content:
+                continue
+
+            if regexp.match(message.content):
+                messages.append(message)
+
+        if len(messages) == 0:
+            await self.bot.say("\N{WHITE EXCLAMATION MARK ORNAMENT} Search results: Found `0` messages.")
+            return
+
+        header = "\N{HEAVY EXCLAMATION MARK SYMBOL} Search results: Found `{}` messages\n".format(len(messages))
+        fmt = "{}\n".format(header)
+
+        messages = list(reversed(messages))
+
+        if len(messages) <= 10:
+            # Just say them in the normal channel.
+            for num, message in enumerate(messages):
+                fmt += "**{}:** `[{}] {}: {}`\n".format(num, message.timestamp,
+                                                        message.author.name.replace("`", "´"),
+                                                        message.clean_content.replace("`", "´"),
+                                                        )
+            await self.bot.say(fmt)
+        else:
+            await self.bot.say("Private messaging you the search results.")
+            sp = [messages[i:i + 10] for i in range(0, len(messages), 10)]
+            # yikes, counters
+            counter = 0
+            for group in sp:
+                msg_block = ""
+                for message in group:
+                    counter += 1
+                    msg_block += "**{}:** `[{}] {}: {}`\n".format(counter, message.timestamp,
+                                                                  message.author.name.replace("`", "´"),
+                                                                  message.clean_content.replace("`", "´"),
+                                                                  )
+                await self.bot.whisper(msg_block)
 
 
 def setup(bot: Chiru):
