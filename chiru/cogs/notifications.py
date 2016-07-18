438b46c2cd855f5c209247e999b38513d9723a48
SunDwarf <sun@veriny.tf>
2016-07-18 22:24:24 +0100

    Change notifications to work on only bans/joins as well as all.

--- chiru/cogs/notifications.py
+++ chiru/cogs/notifications.py
@@ -28,36 +28,54 @@ class Notifications(object):
 
     @notifications.command(pass_context=True)
     @commands.check(checks.has_manage_server)
-    async def on(self, ctx: Context):
+    async def all(self, ctx: Context):
         """
-        Turn notifications on.
+        Turn all notifications on.
         """
-        await ctx.set_config("notifications", "on")
+        await ctx.set_config("notifications", "all")
         await self.bot.say("Notifications turned on.")
 
     @notifications.command(pass_context=True)
     @commands.check(checks.has_manage_server)
     async def off(self, ctx: Context):
         """
-        Turn notifications off.
+        Turn all notifications off.
         """
         await ctx.set_config("notifications", "off")
         await self.bot.say("Notifications turned off.")
 
+    @notifications.command(pass_context=True)
+    @commands.check(checks.has_manage_server)
+    async def bans(self, ctx: Context):
+        """
+        Only notify on bans.
+        """
+        await ctx.set_config("notifications", "bans")
+        await self.bot.say("Notifications set to bans only.")
+
+    @notifications.command(pass_context=True)
+    @commands.check(checks.has_manage_server)
+    async def joins(self, ctx: Context):
+        """
+        Only notify on joins.
+        """
+        await ctx.set_config("notifications", "joins")
+        await self.bot.say("Notifications set to joins only.")
+
     async def on_member_join(self, member: discord.Member):
-        if (await self.bot.get_config(member.server, "notifications")) == "on":
+        if (await self.bot.get_config(member.server, "notifications")) in ["all", "joins"]:
             await self.bot.send_message(member.server.default_channel, "**{}** has joined!".format(member.name))
 
     async def on_member_ban(self, member: discord.Member):
-        if (await self.bot.get_config(member.server, "notifications")) == "on":
+        if (await self.bot.get_config(member.server, "notifications")) == ["all", "bans"]:
             await self.bot.send_message(member.server.default_channel, "**{}** got bent".format(member.name))
 
     async def on_member_unban(self, server: discord.Server, user: discord.User):
-        if (await self.bot.get_config(server, "notifications")) == "on":
+        if (await self.bot.get_config(server, "notifications")) == ["all", "bans"]:
             await self.bot.send_message(server.default_channel, "**{}** got unbanned.".format(user.name))
 
     async def on_member_remove(self, member: discord.Member):
-        if (await self.bot.get_config(member.server, "notifications")) == "on":
+        if (await self.bot.get_config(member.server, "notifications")) == ["all", "joins"]:
             await self.bot.send_message(member.server.default_channel, "**{}** left".format(member.name))
 
 
