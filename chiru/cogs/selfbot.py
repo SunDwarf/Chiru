65f3ef91bb7e4c2d47f32871d8b73d4efc6c316d
SunDwarf <sun@veriny.tf>
2016-07-20 09:36:57 +0100

    Add be a dick command.

--- chiru/cogs/selfbot.py
+++ chiru/cogs/selfbot.py
@@ -1,9 +1,12 @@
 """
 Self-bot related stuff.
 """
+import discord
 from discord.ext import commands
 
 from bot import Chiru
+from chiru import util
+from override import Context
 
 
 class SelfBot(object):
@@ -15,6 +18,15 @@ class SelfBot(object):
         with open("selfbot/93591378994397184.png", 'rb') as f:
             await self.bot.send_file(ctx.channel, f, filename="u200b.png")
 
+    @commands.command(pass_context=True)
+    async def beadick(self, ctx: Context):
+        """
+        Mention everyone in the channel without using @ everyone.
+        """
+        mentions = " ".join([member.mention for member in ctx.server.members])
+        chunked = util.chunk(mentions)
+        for chunk in chunked:
+            await self.bot.say(chunk)
 
 def setup(bot: Chiru):
     bot.add_cog(SelfBot(bot))
