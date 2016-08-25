056cf5bb4b2edb40e8e183643751267d1389ba08
SunDwarf <sun@veriny.tf>
2016-08-25 16:38:49 +0100

    Add `addbot` command which adds a new bot to my server.

--- chiru/cogs/fuyu.py
+++ chiru/cogs/fuyu.py
@@ -1,9 +1,12 @@
 """
 Cog used for my server.
 """
+import json
 import random
 
 import datetime
+
+import aiohttp
 import discord
 import time
 from discord.ext import commands
@@ -11,6 +14,8 @@ from discord.ext import commands
 from bot import Chiru
 from chiru import checks
 from override import Context
+
+AUTHORIZATION_URL = "https://discordapp.com/api/v6/oauth2/authorize"
 
 
 class Fuyu:
@@ -43,6 +48,40 @@ class Fuyu:
 
         await self.bot.add_roles(chosen, role)
         await self.bot.say("{} has got the abuse role! Hooray!".format(chosen.mention))
+
+    @commands.command(pass_context=True)
+    async def addbot(self, ctx: Context, client_id: int):
+        """
+        Adds a bot to my server.
+
+        This requires your client ID.
+        Your bot will be added with no permissions.
+        """
+        payload = {
+            "guild_id": ctx.server.id,
+            "authorize": True,
+            "permissions": 0
+        }
+
+        headers = {
+            "Authorization": self.bot.config.get("bot_add_token"),
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36",
+            "Content-Type": "application/json"
+        }
+        async with aiohttp.ClientSession() as sess:
+            response = await sess.post(AUTHORIZATION_URL,
+                                       params={"client_id": client_id,
+                                               "scope": "bot"},
+                                       headers=headers,
+                                       data=json.dumps(payload))
+            assert isinstance(response, aiohttp.ClientResponse)
+            status = response.status
+            js = await response.json()
+            if status != 200:
+                await self.bot.say("\N{NO ENTRY SIGN} Failed to add bot to server! Error `{}`".format(status))
+            else:
+                await self.bot.say("\N{THUMBS UP SIGN} Added new bot.")
 
 
 def setup(bot: Chiru):
